de02f41c085bfab405817d25f0a4c4d2881a36af
pedrorhamon <pedrorhamon16@gmail.com>
2022-06-12 14:10:06 -0300

    Ajustando o Thymeleaf view

--- src/main/java/com/starking/cerveja/controllers/CervejaController.java
+++ src/main/java/com/starking/cerveja/controllers/CervejaController.java
@@ -1,8 +1,14 @@
 package com.starking.cerveja.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejaController {
+	
+	@RequestMapping("/cervejas/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
 
 }
