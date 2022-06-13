a56a4d3ee96855c104669f3b71d66e4d317d40e1
pedrorhamon <pedrorhamon16@gmail.com>
2022-06-12 21:35:52 -0300

    Começando o cadastro de cerveja

--- src/main/java/com/starking/cerveja/controllers/CervejaController.java
+++ src/main/java/com/starking/cerveja/controllers/CervejaController.java
@@ -2,6 +2,7 @@ package com.starking.cerveja.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CervejaController {
@@ -9,6 +10,11 @@ public class CervejaController {
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
 		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cerveja/novo" , method = RequestMethod.POST)
+	public void cadastrar() {
+		
 	}
 
 }
