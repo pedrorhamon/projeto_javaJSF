05f9eddf410eeb3bc578c0073de47fb5f1aba9cc
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-06-25 21:30:47 -0300

    tirando o TODO

--- src/main/java/com/starking/cerveja/config/init/AppInicializer.java
+++ src/main/java/com/starking/cerveja/config/init/AppInicializer.java
@@ -11,7 +11,7 @@ public class AppInicializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
