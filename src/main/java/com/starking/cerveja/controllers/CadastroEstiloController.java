9cb01a0fedd7edd0ce2f359b485e7f7f4c404c27
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-04 22:03:04 -0300

    configurando o log4j2

--- src/main/java/com/starking/cerveja/controllers/CadastroEstiloController.java
+++ src/main/java/com/starking/cerveja/controllers/CadastroEstiloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CadastroEstilo {
+public class CadastroEstiloController {
 	
 	@RequestMapping("/estilos/novo")
 	public String novo() {
