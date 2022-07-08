747944366d03430d2fef0e1d1e7c61e98725f570
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-07 21:57:15 -0300

    repository

--- src/main/java/com/starking/cerveja/controllers/CervejaController.java
+++ src/main/java/com/starking/cerveja/controllers/CervejaController.java
@@ -1,7 +1,10 @@
 package com.starking.cerveja.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,21 +13,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.starking.cerveja.model.Cerveja;
+import com.starking.cerveja.repositories.CervejaRepository;
 
 @Controller
 public class CervejaController {
 	
+	@Autowired
+	private CervejaRepository cervejaRepository;
+	
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
+//		Optional<Cerveja> cervejaOptional = cervejaRepository.findBySkuIgnoreCase("111154");
+		
 		return "cerveja/CadastroCerveja";
 	}
 	
-	@RequestMapping(value = "/cerveja/novo" , method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
-		if(result.hasErrors()) {
-			return novo(cerveja);
-		}
-		redirectAttributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
-		return "redirect:/cerveja/novo";
-	}
+//	@RequestMapping(value = "/cerveja/novo" , method = RequestMethod.POST)
+//	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+//		if(result.hasErrors()) {
+//			return novo(cerveja);
+//		}
+//		redirectAttributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
+//		return "redirect:/cerveja/novo";
+//	}
 }
--- src/main/java/com/starking/cerveja/repositories/CervejaRepository.java
+++ src/main/java/com/starking/cerveja/repositories/CervejaRepository.java
@@ -1,5 +1,7 @@
 package com.starking.cerveja.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.starking.cerveja.model.Cerveja;
 
 @Repository
 public interface CervejaRepository extends JpaRepository<Cerveja, Long>{
+	
+//	Optional<Cerveja> findBySkuIgnoreCase(String sku);
 
 }
