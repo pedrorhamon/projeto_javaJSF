f78d0434fb80115a0eadd03aa138679929020744
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-10 09:49:48 -0300

    Mostrando mensagems de cadastro

--- src/main/java/com/starking/cerveja/model/Cerveja.java
+++ src/main/java/com/starking/cerveja/model/Cerveja.java
@@ -34,7 +34,7 @@ public class Cerveja {
 	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
 
-	@Size(max = 50, message = "A descrição deve estar entre 1 e 50")
+	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
 	private String descricao;
 
 	private BigDecimal valor;
