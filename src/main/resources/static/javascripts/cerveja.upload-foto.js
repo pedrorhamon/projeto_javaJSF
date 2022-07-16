83ea40ccb46ba9ba7948730cfc1c714d4ab96e80
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-16 00:14:41 -0300

    Mantendo a cerveja na validação

--- src/main/resources/static/javascripts/cerveja.upload-foto.js
+++ src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -25,6 +25,10 @@ Cerveja.UploadFoto = (function() {
 		
 		UIkit.uploadSelect($('#upload-select'), settings);
 		UIkit.uploadDrop(this.uploadDrop, settings);
+		
+		if(this.inputNomeFoto.val()) {
+			onUploadCompleto.call({nome : this.inputNomeFoto.val(), contentType: this.inputContentType.val()});
+		} 
 	}
 	
 	function onUploadCompleto(resposta) {
