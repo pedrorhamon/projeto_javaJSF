8f6b4ec0b02b170ad9cd3b8bb620afc87d165c18
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-24 15:38:12 -0300

    Mascara de telefone para cliente

--- src/main/resources/static/javascripts/vendors/cerveja.js
+++ src/main/resources/static/javascripts/vendors/cerveja.js
@@ -16,7 +16,33 @@ Cerveja.MaskMoney = (function() {
 	
 }());
 
+Cerveja.MaskPhoneNumber = (function(){
+	
+	function MaskPhoneNumber() {
+		this.inputPhoneNumber = $('.js-phone-number');
+	}
+	
+	MaskPhoneNumber.prototype.enable = function() {
+		var SPMaskBehavior = function(val) {
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(000) 0000-00009';
+		},
+		spOptions = {
+			onKeyPress: function(val, e, field, options) {
+				field.mask(SPMaskBehavior.apply({}, arguments), options);
+			}
+		};	
+		
+		this.inputPhoneNumber.mask(SPMaskBehavior, spOptions);
+	}
+	
+	return MaskPhoneNumber;
+	
+}());
+
 $(function() {
 	var maskNumber = new Cerveja.MaskMoney();
 	maskNumber.enable();
+	
+	var maskPhoneNumber = new Cerveja.MaskPhoneNumber();
+	maskPhoneNumber.enable();
 });
