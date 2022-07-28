b86fdb5760f793b829008f76182a5ea9227c3aec
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-28 19:58:33 -0300

    implementando ajax na chamada de comboCidade e comboEstado

--- src/main/java/com/starking/cerveja/controllers/CidadeController.java
+++ src/main/java/com/starking/cerveja/controllers/CidadeController.java
@@ -1,13 +1,39 @@
 package com.starking.cerveja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.starking.cerveja.model.Cidade;
+import com.starking.cerveja.repositories.CidadeRepository;
+
+
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadeController {
 	
-	@RequestMapping("/cidades/novo")
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@RequestMapping("/novo")
 	public String novo() {
 		return "cidade/CadastroCidade";
 	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(@RequestParam(name ="estado", defaultValue = "-1" ) Long estadoId) {
+		return this.cidadeRepository.findByEstadoCodigo(estadoId);
+	}
+	
+	@RequestMapping
+	public String pesquisar() {
+		return "cidade/CadastroCidade";
+	}
+	
 }
