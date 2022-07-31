1850eda550a13d4898abb24d446b7913c11d5181
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-31 20:28:12 -0300

    resolvendo problema com banco flyway e a chamada de estados com a cidades na tela de cliente

--- src/main/java/com/starking/cerveja/controllers/ClienteController.java
+++ src/main/java/com/starking/cerveja/controllers/ClienteController.java
@@ -1,19 +1,25 @@
 package com.starking.cerveja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.starking.cerveja.model.enums.TipoPessoa;
+import com.starking.cerveja.repositories.EstadoRepository;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	@RequestMapping("novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.addObject("estados", estadoRepository.findAll());
 		return mv;
 	}
 }
