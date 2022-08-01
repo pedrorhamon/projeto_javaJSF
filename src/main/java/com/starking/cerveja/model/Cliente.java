a4ffd7e3630d8c7069cd1186a1aac0949277a698
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-07-31 22:06:37 -0300

    Mapeando a entidade endereco

--- src/main/java/com/starking/cerveja/model/Cliente.java
+++ src/main/java/com/starking/cerveja/model/Cliente.java
@@ -4,14 +4,18 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.starking.cerveja.model.enums.TipoPessoa;
 
+@Entity
+@Table(name = "cliente")
 public class Cliente implements Serializable{
 
 	private static final long serialVersionUID = 1L;
