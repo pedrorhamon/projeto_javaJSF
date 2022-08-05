cbaf152fbf05744c34fa05d626ea66697b0a0af1
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-08-04 23:55:44 -0300

    CacheManager simples

--- src/main/java/com/starking/cerveja/controllers/CidadeController.java
+++ src/main/java/com/starking/cerveja/controllers/CidadeController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
@@ -64,6 +65,7 @@ public class CidadeController {
 		return new ModelAndView("redirect:/cidades/novo");
 	}
 	
+	@Cacheable("cidades")
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(@RequestParam(name ="estado", defaultValue = "-1" ) Long codigoEstado) {
 		try {
