702c4b8c2418758f2072f0857f79cdaa08c3c010
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-08-11 00:00:49 -0300

    Configurando o CSRF

--- src/main/java/com/starking/cerveja/config/SecurityConfig.java
+++ src/main/java/com/starking/cerveja/config/SecurityConfig.java
@@ -45,9 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginPage("/login")
 				.permitAll()
 				.and()
-				.exceptionHandling().accessDeniedPage("/403")
-				.and()
-			.csrf().disable();
+				.exceptionHandling().accessDeniedPage("/403");
+				
 	}
 	
 	@Bean
