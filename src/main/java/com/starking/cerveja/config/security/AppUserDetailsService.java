7eddb0d5942a8882af5b5fd3b3ab89acda5091ca
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-08-12 21:05:48 -0300

    exibindo o usuario logado

--- src/main/java/com/starking/cerveja/config/security/AppUserDetailsService.java
+++ src/main/java/com/starking/cerveja/config/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,10 +25,8 @@ public class AppUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = this.usuarioRepository.porEmailEAtivo(email);
-		
 		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-		
-		return new User(usuario.getEmail(), usuario.getSenha(), getPermissao(usuario));
+		return new UsuarioSystem(usuario, getPermissao(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissao(Usuario usuario) {
