477b6fb3166ce4d39a80745ac89598eae04bf01e
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-08-14 22:35:56 -0300

    filtro com criteria de grupo de usuario

---
+++ src/main/java/com/starking/cerveja/repositories/filter/UsuarioFilter.java
@@ -0,0 +1,33 @@
+package com.starking.cerveja.repositories.filter;
+
+import java.util.List;
+
+import com.starking.cerveja.model.Grupo;
+
+public class UsuarioFilter {
+	
+	private String nome;
+	private String email;
+	private List<Grupo> grupos;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+	
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
+	}
+}
